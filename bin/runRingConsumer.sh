c357d354a7bf3238ee7cd3dd29412ccc7d58a171
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2024-11-12 08:34:40 -0300

    Add run scripts

---
+++ bin/runRingConsumer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source ./bin/checkJvmVersion.sh
+
+ADD_OPENS=""
+
+if (( major_version > 8 )); then
+    ADD_OPENS="--add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED"
+fi
+
+CMD="java $ADD_OPENS -cp target/classes:target/coralring-all.jar com.coralblocks.coralring.example.ring.Consumer"
+
+echo
+echo $CMD
+echo
+
+$CMD
+
+echo
+
