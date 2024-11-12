081c70137b57ffd615850f6f2c46941c87e43381
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2024-11-12 13:28:27 -0300

    Only test with java 16, 17, 21 and 23

--- bin/runSharedMemoryTest-All-Java-Versions.sh
+++ bin/runSharedMemoryTest-All-Java-Versions.sh
@@ -2,8 +2,6 @@
 
 shopt -s expand_aliases
 
-alias java8='sudo rm /Library/Java/JavaVirtualMachines/Default; sudo ln -sf /Library/Java/JavaVirtualMachines/jdk8-oracle /Library/Java/JavaVirtualMachines/Default'
-alias java11='sudo rm /Library/Java/JavaVirtualMachines/Default; sudo ln -sf /Library/Java/JavaVirtualMachines/jdk11-zulu /Library/Java/JavaVirtualMachines/Default'
 alias java16='sudo rm /Library/Java/JavaVirtualMachines/Default; sudo ln -sf /Library/Java/JavaVirtualMachines/jdk16-zulu /Library/Java/JavaVirtualMachines/Default'
 alias java17='sudo rm /Library/Java/JavaVirtualMachines/Default; sudo ln -sf /Library/Java/JavaVirtualMachines/jdk17-oracle /Library/Java/JavaVirtualMachines/Default'
 alias java19='sudo rm /Library/Java/JavaVirtualMachines/Default; sudo ln -sf /Library/Java/JavaVirtualMachines/jdk19-oracle /Library/Java/JavaVirtualMachines/Default'
@@ -11,27 +9,11 @@ alias java20='sudo rm /Library/Java/JavaVirtualMachines/Default; sudo ln -sf /Li
 alias java21='sudo rm /Library/Java/JavaVirtualMachines/Default; sudo ln -sf /Library/Java/JavaVirtualMachines/jdk21-oracle /Library/Java/JavaVirtualMachines/Default'
 alias java23='sudo rm /Library/Java/JavaVirtualMachines/Default; sudo ln -sf /Library/Java/JavaVirtualMachines/jdk23-oracle /Library/Java/JavaVirtualMachines/Default'
 
-java8
-
-./bin/runSharedMemoryTest.sh
-
-java11
-
-./bin/runSharedMemoryTest.sh
-
 java16
 
 ./bin/runSharedMemoryTest.sh
 
 java17
-
-./bin/runSharedMemoryTest.sh
-
-java19
-
-./bin/runSharedMemoryTest.sh
-
-java20
 
 ./bin/runSharedMemoryTest.sh
 
@@ -43,5 +25,5 @@ java23
 
 ./bin/runSharedMemoryTest.sh
 
-java8
+java16
 
