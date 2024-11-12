331a2c2f10fb9ea74b8d7f2cc5f5ec61d2b0ad96
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2024-11-12 19:41:21 -0300

    Adding sleep time

--- bin/runRingConsumer.sh
+++ bin/runRingConsumer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 EXPECTED_MESSAGES_TO_RECEIVE=${1:-100000}
+SLEEP_TIME=${2:-5000000}
 
 source ./bin/checkJvmVersion.sh
 
@@ -10,7 +11,7 @@ if (( major_version > 8 )); then
     ADD_OPENS="--add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED"
 fi
 
-CMD="java $ADD_OPENS -cp target/classes:target/coralring-all.jar com.coralblocks.coralring.example.ring.Consumer $EXPECTED_MESSAGES_TO_RECEIVE"
+CMD="java $ADD_OPENS -cp target/classes:target/coralring-all.jar com.coralblocks.coralring.example.ring.Consumer $EXPECTED_MESSAGES_TO_RECEIVE $SLEEP_TIME"
 
 echo
 echo $CMD
