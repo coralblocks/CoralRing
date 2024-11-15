91aedcb3cd35197477d727c2fe1a9a6af1de0886
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2024-11-15 19:22:07 -0300

    Adding payload ByteBuffer message

---
+++ src/main/java/com/coralblocks/coralring/util/PayloadByteBufferMessage.java
@@ -0,0 +1,44 @@
+package com.coralblocks.coralring.util;
+
+import java.nio.ByteBuffer;
+
+import com.coralblocks.coralring.memory.Memory;
+
+public class PayloadByteBufferMessage implements MemorySerializable {
+	
+	private static final boolean DEFAULT_USE_DIRECT_BYTE_BUFFER = true;
+	
+	public int payloadLength;
+	public final ByteBuffer payload;
+	
+	public final static int getMaxSize(int maxPayloadSize) {
+		return 4 /* payloadLength int size */ + maxPayloadSize;
+	}
+	
+	public PayloadByteBufferMessage(int maxPayloadSize, boolean useDirectByteBuffer) {
+		if (useDirectByteBuffer) {
+			this.payload = ByteBuffer.allocateDirect(maxPayloadSize);
+		} else {
+			this.payload = ByteBuffer.allocate(maxPayloadSize);
+		}
+	}
+	
+	public PayloadByteBufferMessage(int maxPayloadSize) {
+		this(maxPayloadSize, DEFAULT_USE_DIRECT_BYTE_BUFFER);
+	}
+
+	@Override
+	public void writeTo(long pointer, Memory memory) {
+		memory.putInt(pointer, payloadLength);
+		payload.limit(payloadLength).position(0);
+		memory.putByteBuffer(pointer + 4, payload, payloadLength);
+	}
+
+	@Override
+	public void readFrom(long pointer, Memory memory) {
+		this.payloadLength = memory.getInt(pointer);
+		memory.getByteBuffer(pointer + 4, payload, payloadLength);
+	}
+	
+	
+}
