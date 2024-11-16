63c3a0722c57c3e2c126d1294f20ac81876ffe07
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2024-11-15 21:49:13 -0300

    Only use direct buffer

--- src/main/java/com/coralblocks/coralring/util/PayloadByteBufferMessage.java
+++ src/main/java/com/coralblocks/coralring/util/PayloadByteBufferMessage.java
@@ -6,8 +6,6 @@ import com.coralblocks.coralring.memory.Memory;
 
 public class PayloadByteBufferMessage implements MemorySerializable {
 	
-	private static final boolean DEFAULT_USE_DIRECT_BYTE_BUFFER = true;
-	
 	public int payloadLength;
 	public final ByteBuffer payload;
 	
@@ -15,18 +13,10 @@ public class PayloadByteBufferMessage implements MemorySerializable {
 		return 4 /* payloadLength int size */ + maxPayloadSize;
 	}
 	
-	public PayloadByteBufferMessage(int maxPayloadSize, boolean useDirectByteBuffer) {
-		if (useDirectByteBuffer) {
-			this.payload = ByteBuffer.allocateDirect(maxPayloadSize);
-		} else {
-			this.payload = ByteBuffer.allocate(maxPayloadSize);
-		}
+	public PayloadByteBufferMessage(int maxPayloadSize) {
+		this.payload = ByteBuffer.allocateDirect(maxPayloadSize);
 	}
 	
-	public PayloadByteBufferMessage(int maxPayloadSize) {
-		this(maxPayloadSize, DEFAULT_USE_DIRECT_BYTE_BUFFER);
-	}
-
 	@Override
 	public void writeTo(long pointer, Memory memory) {
 		memory.putInt(pointer, payloadLength);
