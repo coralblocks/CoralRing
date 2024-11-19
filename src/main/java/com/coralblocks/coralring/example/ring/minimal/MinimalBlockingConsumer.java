fc0e8e7d0000a47dbedfc6e2754d3225681ce771
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2024-11-19 10:55:19 -0300

    Adding minimal blocking example

---
+++ src/main/java/com/coralblocks/coralring/example/ring/minimal/MinimalBlockingConsumer.java
@@ -0,0 +1,42 @@
+package com.coralblocks.coralring.example.ring.minimal;
+
+import com.coralblocks.coralring.ring.BlockingRingConsumer;
+import com.coralblocks.coralring.ring.RingConsumer;
+
+public class MinimalBlockingConsumer {
+	
+	private final static String FILENAME = MinimalBlockingProducer.FILENAME;
+	
+	public static void main(String[] args) {
+		
+		final int messagesToSend = 10;
+		
+		final RingConsumer<MutableLong> ringConsumer = new BlockingRingConsumer<MutableLong>(MutableLong.getMaxSize(), MutableLong.class, FILENAME); // default size is 1024
+		
+		boolean isRunning = true;
+		
+		while(isRunning) {
+			
+			long avail = ringConsumer.availableToPoll(); // read available batches as fast as possible
+			
+			if (avail == 0) continue; // busy spin
+			
+			for(int i = 0; i < avail; i++) {
+				
+				MutableLong ml = ringConsumer.poll();
+				
+				System.out.print(ml.get());
+				
+				if (ml.get() == messagesToSend - 1) isRunning = false; // done receiving all messages
+			}
+			
+			ringConsumer.donePolling(); // don't forget to notify producer
+		}
+		
+		ringConsumer.close(true);
+		
+		System.out.println();
+		
+		// OUTPUT: 0123456789
+	}
+}
---
+++ src/main/java/com/coralblocks/coralring/example/ring/minimal/MutableLong.java
@@ -0,0 +1,90 @@
+/* 
+ * Copyright 2024 (c) CoralBlocks - http://www.coralblocks.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package com.coralblocks.coralring.example.ring.minimal;
+
+import com.coralblocks.coralring.memory.Memory;
+import com.coralblocks.coralring.util.MemorySerializable;
+
+/**
+ * A mutable long class to encapsulate a long value.
+ */
+public class MutableLong implements MemorySerializable {
+	
+	private final static int LONG_SIZE = 8; // 8 bytes
+	
+	private long value;
+	
+	/**
+	 * Constructs a new <code>MutableLong</code> with the given long value.
+	 *
+	 * @param value the initial long value
+	 */
+	public MutableLong(long value) {
+		set(value);
+	}
+	
+	/**
+	 * Constructs a new <code>MutableLong</code> with a value of zero.
+	 */
+	public MutableLong() {
+		this(0);
+	}
+	
+	/**
+	 * Changes the value of this <code>MutableLong</code>.
+	 *
+	 * @param value the new long value
+	 */
+	public void set(long value) {
+		this.value = value;
+	}
+	
+	/**
+	 * Gets the current long value.
+	 *
+	 * @return the long value
+	 * @throws NullPointerException if the value is null
+	 */
+	public long get() {
+		return value;
+	}
+	
+	/**
+	 * Return the max size of this object
+	 * 
+	 * @return the max size of this object
+	 */
+	public static int getMaxSize() {
+		return LONG_SIZE;
+	}
+	
+	@Override
+	public int writeTo(long pointer, Memory memory) {
+		memory.putLong(pointer, value);
+		return LONG_SIZE;
+	}
+
+	@Override
+	public int readFrom(long pointer, Memory memory) {
+		value = memory.getLong(pointer);
+		return LONG_SIZE;
+	}
+	
+	@Override
+	public String toString() {
+		return String.valueOf(value);
+	}
+}
