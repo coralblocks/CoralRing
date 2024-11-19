ce04c69559b18eba22dfb6b134ea49eccf268dbc
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2024-11-19 10:58:16 -0300

    Adding minimal non-blocking example

---
+++ src/main/java/com/coralblocks/coralring/example/ring/minimal/MinimalNonBlockingProducer.java
@@ -0,0 +1,31 @@
+package com.coralblocks.coralring.example.ring.minimal;
+
+import com.coralblocks.coralring.ring.NonBlockingRingProducer;
+import com.coralblocks.coralring.ring.RingProducer;
+
+public class MinimalNonBlockingProducer {
+	
+	final static String FILENAME = "minimal-blocking.mmap";
+	
+	public static void main(String[] args) {
+		
+		final int messagesToSend = 10;
+		
+		final RingProducer<MutableLong> ringProducer = new NonBlockingRingProducer<MutableLong>(MutableLong.getMaxSize(), MutableLong.class, FILENAME); // default size is 1024
+		
+		for(int i = 0; i < messagesToSend; i += 2) { // note we are looping 2 by 2 (we are sending a batch of 2 messages)
+			
+			MutableLong ml; // our data transfer mutable object
+			
+			while((ml = ringProducer.nextToDispatch()) == null); // busy spin
+			ml.set(i);
+			
+			while((ml = ringProducer.nextToDispatch()) == null); // busy spin
+			ml.set(i + 1);
+			
+			ringProducer.flush(); // don't forget to notify consumer
+		}
+		
+		ringProducer.close(false);
+	}
+}
